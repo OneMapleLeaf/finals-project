4971f0e6c3d4a3bd78e975276bcea30468b87c73
James Pandio <114295563+OneMapleLeaf@users.noreply.github.com>
2024-05-10 16:18:31 +0800

    added samples

--- index.js
+++ index.js
@@ -13,12 +13,15 @@ const openShowCase = () => {
 
 document.addEventListener('click', e => {
   const isDropdownButton = e.target.matches('#dropdown_btn');
+  console.log('Clicked element: ', e.target); // Logs the clicked element
+  console.log('Is dropdown button: ', isDropdownButton);
   if (!isDropdownButton && e.target.closest('#dropdown') != null) return;
 
   let curDropDown;
   if (isDropdownButton){
     curDropDown = e.target.closest('#dropdown');
     curDropDown.classList.toggle('active');
+    console.log('Current dropdown: ', curDropDown);
   }
 
   document.querySelectorAll('#dropdown.active').forEach(dropdown => {
